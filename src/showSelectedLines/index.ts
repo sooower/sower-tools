cdbed31155c4911c6d6256826cc2b8b20ae7a0f2
Jian Liu <jian.liu@fomopay.com>
2024-06-01 00:05:13 +0800

    feat: supports show selected code lines

--- src/showSelectedLines/index.ts
+++ src/showSelectedLines/index.ts
@@ -1,3 +1,5 @@
+import { format } from "node:util";
+
 import { StatusBarAlignment, StatusBarItem, window } from "vscode";
 
 import { extensionCtx } from "../shared";
@@ -5,26 +7,82 @@ import { extensionCtx } from "../shared";
 let statusBarItem: StatusBarItem;
 
 export async function subscribeShowSelectedLines() {
+    const activatedEditor = window.activeTextEditor;
+    if (activatedEditor === undefined) {
+        return;
+    }
+    const languageId = activatedEditor.document.languageId;
+
+    // Only support js and ts by now
+    if (languageId !== "javascript" && languageId !== "typescript") {
+        return;
+    }
+
     statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 100);
-    showSelectedLines(0);
+    showSelectedLines({});
     statusBarItem.show();
 
     window.onDidChangeTextEditorSelection((event) => {
-        if (event.selections.length > 0) {
-            const [selection] = event.selections;
-            showSelectedLines(
-                selection.isEmpty
-                    ? 0
-                    : selection.end.line - selection.start.line + 1
-            );
-        } else {
-            showSelectedLines(0);
+        const [selection] = event.selections;
+        if (selection.isEmpty) {
+            return showSelectedLines({});
         }
+
+        const selectedText = activatedEditor.document
+            .getText(activatedEditor.selection)
+            .split(/\r?\n/);
+
+        let lines = 0;
+        let code = 0;
+        const indexOfSelectedLineStart = selection.start.line;
+        const indexOfSelectedLineEnd = selection.end.line;
+        for (
+            let i = indexOfSelectedLineStart;
+            i <= indexOfSelectedLineEnd;
+            i++
+        ) {
+            const currLineText = activatedEditor.document.lineAt(i).text;
+            const currSelectedLineText =
+                selectedText[i - indexOfSelectedLineStart];
+            if (
+                currSelectedLineText.trim() === "" &&
+                (i === indexOfSelectedLineStart || i === indexOfSelectedLineEnd)
+            ) {
+                if (currSelectedLineText.length > 0) {
+                    lines++;
+                }
+                continue;
+            }
+
+            // Comment or blank line
+            if (
+                currLineText.trim() === "" ||
+                currLineText.trimStart().startsWith("//") ||
+                currLineText.trimStart().startsWith("/*") ||
+                currLineText.trimEnd().endsWith("*/") ||
+                currLineText.trimStart().startsWith("*")
+            ) {
+                lines++;
+                continue;
+            }
+
+            lines++;
+            code++;
+        }
+
+        showSelectedLines({
+            lines,
+            code,
+        });
     });
 
     extensionCtx.subscriptions.push(statusBarItem);
 }
 
-function showSelectedLines(line: number) {
-    statusBarItem.text = `Selected Lines: ${line}`;
+function showSelectedLines({ lines, code }: { lines?: number; code?: number }) {
+    statusBarItem.text = format(
+        `Selected Lines:%s, Code:%s`,
+        lines ?? 0,
+        code ?? 0
+    );
 }
